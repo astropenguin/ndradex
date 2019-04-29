fe2607fb753f25bd86b4178cacee36a9bd6e5df5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-04-30 02:12:35 +0900

    :+1: Update dims (reorder, delete geo)

--- ndradex/radex.py
+++ ndradex/radex.py
@@ -12,9 +12,8 @@ import ndradex as nd
 # module constants
 class Dims(Enum):
     QN_ul = 'QN_ul'
-    N_mol = 'N_mol'
     T_kin = 'T_kin'
-    T_bg  = 'T_bg'
+    N_mol = 'N_mol'
     n_H2  = 'n_H2'
     n_pH2 = 'n_p-H2'
     n_oH2 = 'n_o-H2'
@@ -22,8 +21,8 @@ class Dims(Enum):
     n_H   = 'n_H'
     n_He  = 'n_He'
     n_Hp  = 'n_H+'
+    T_bg  = 'T_bg'
     dv    = 'dv'
-    geo   = 'geo'
 
 class Vars(Enum):
     E_u   = 'E_u'
@@ -40,11 +39,11 @@ class Vars(Enum):
 
 # main function
 def calc(moldata, QN_ul, T_kin=100, T_bg=2.73, N_mol=1e15,
-         n_H2=1e3, dv=1.0, geo='uni', n_pH2=None, n_oH2=None,
-         n_e=None, n_H=None, n_He=None, n_Hp=None, squeeze=True):
+         n_H2=1e3, dv=1.0, n_pH2=None, n_oH2=None, n_e=None,
+         n_H=None, n_He=None, n_Hp=None, geometry='uni'):
     """Execute N-dim RADEX calculation."""
-    coords = _get_coords(QN_ul, T_kin, T_bg, N_mol, n_H2, dv, geo,
-                         n_pH2, n_oH2, n_e, n_H, n_He, n_Hp)
+    coords = _get_coords(QN_ul, T_kin, N_mol, n_H2, n_pH2,
+                         n_oH2, n_e, n_H, n_He, n_Hp, T_bg, dv)
 
     with TemporaryDirectory(dir='.') as tempdir:
         with nd.LAMDA(moldata, tempdir) as lamda:
@@ -80,13 +79,12 @@ def _run_radex(input, geometry='uni'):
     pass
 
 
-def _get_coords(QN_ul, T_kin=100, T_bg=2.73, N_mol=1e15, n_H2=1e3,
-                 dv=1.0, geo='uni', n_pH2=None, n_oH2=None,
-                 n_e=None, n_H=None, n_He=None, n_Hp=None):
+def _get_coords(QN_ul, T_kin=100, N_mol=1e15, n_H2=1e3,
+                n_pH2=None, n_oH2=None, n_e=None, n_H=None,
+                n_He=None, n_Hp=None, T_bg=2.73, dv=1.0):
     """Make coords as list for xarray.DataArray objects."""
     items = {Dims.QN_ul: nd.ensure_values(QN_ul),
              Dims.T_kin: nd.ensure_values(T_kin, 'K'),
-             Dims.T_bg:  nd.ensure_values(T_bg, 'K'),
              Dims.N_mol: nd.ensure_values(N_mol, 'cm^-2'),
              Dims.n_H2:  nd.ensure_values(n_H2, 'cm^-3'),
              Dims.n_pH2: nd.ensure_values(n_pH2, 'cm^-3'),
@@ -95,8 +93,8 @@ def _get_coords(QN_ul, T_kin=100, T_bg=2.73, N_mol=1e15, n_H2=1e3,
              Dims.n_H:   nd.ensure_values(n_H, 'cm^-3'),
              Dims.n_He:  nd.ensure_values(n_He, 'cm^-3'),
              Dims.n_Hp:  nd.ensure_values(n_Hp, 'cm^-3'),
-             Dims.dv:    nd.ensure_values(dv, 'km/s'),
-             Dims.geo:   nd.ensure_values(geo)}
+             Dims.T_bg:  nd.ensure_values(T_bg, 'K'),
+             Dims.dv:    nd.ensure_values(dv, 'km/s')}
 
     return [(dim.value, items[dim]) for dim in Dims]
 
