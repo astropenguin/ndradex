8220050ca190c9540a73f5c7e1b12fb8deb1a18d
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-04-30 01:33:07 +0900

    :sparkles: Add enum dims and vars

--- ndradex/radex.py
+++ ndradex/radex.py
@@ -1,10 +1,39 @@
 # from standard library
+from enum import Enum
 from pathlib import Path
 from subprocess import run, PIPE
 
 # from dependent packages
 import numpy as np
 import xarray as xr
+
+# module constants
+class Dims(Enum):
+    QN_ul = 'QN_ul'
+    N_mol = 'N_mol'
+    T_kin = 'T_kin'
+    T_bg  = 'T_bg'
+    n_H2  = 'n_H2'
+    n_pH2 = 'n_p-H2'
+    n_oH2 = 'n_o-H2'
+    n_e   = 'n_e'
+    n_H   = 'n_H'
+    n_He  = 'n_He'
+    n_Hp  = 'n_H+'
+    dv    = 'dv'
+    geo   = 'geo'
+
+class Vars(Enum):
+    E_u   = 'E_u'
+    freq  = 'freq'
+    wavel = 'wavel'
+    T_ex  = 'T_ex'
+    tau   = 'tau'
+    T_r   = 'T_r'
+    pop_u = 'pop_u'
+    pop_l = 'pop_l'
+    I     = 'I'
+    F     = 'F'
 
 
 # main function
