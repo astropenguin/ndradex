c0d12e9115273ad31ef3410bc766410df30c473f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-10-03 01:02:01 +0900

    #11 Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
-# from standard library
+# standard library
 from distutils.command.build import build
 from subprocess import run as sprun
 from subprocess import PIPE
 
-# from dependent packages
+
+# dependencies
 from setuptools import setup
 
-# module constants
-RADEX_BUILD = ['make', 'build', 'LOGFILE=/dev/null', 'MAXITER=999999']
 
-# custom build
+# constants
+RADEX_BUILD = [
+    "make",
+    "build",
+    "LOGFILE=/dev/null",
+    "MAXITER=999999",
+]
+
+
+# custom build class
 class Build(build):
     def run(self):
-        cp = sprun(RADEX_BUILD, stdout=PIPE, stderr=PIPE,
-                   cwd='ndradex/bin', check=True)
+        sprun(
+            RADEX_BUILD,
+            stdout=PIPE,
+            stderr=PIPE,
+            cwd="ndradex/bin",
+            check=True,
+        )
         build.run(self)
 
 
-# main function
-if __name__ == '__main__':
-    setup(cmdclass={'build': Build})
+# main script
+if __name__ == "__main__":
+    setup(cmdclass=dict(build=build))
