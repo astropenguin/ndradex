f8f0896a350a99a9f8deee8255347037fc9ab499
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-10-05 14:39:21 +0900

    #19 Add type hints to utils

--- ndradex/utils.py
+++ ndradex/utils.py
@@ -5,27 +5,34 @@ __all__ = [
 
 
 # standard library
-from concurrent import futures
+from concurrent.futures import ProcessPoolExecutor
 from functools import wraps
-from inspect import signature
-from logging import getLogger
+from inspect import signature, Signature
+from logging import getLogger, Logger
 from multiprocessing import cpu_count
+from pathlib import Path
 from random import getrandbits
+from typing import Callable, Optional, Union
 
 
 # dependencies
 import xarray as xr
 
 
-logger = getLogger(__name__)
+# type aliases
+PathLike = Union[Path, str]
 
 
-# utility classes
+# logger
+logger: Logger = getLogger(__name__)
+
+
+# main features
 class set_defaults:
-    def __init__(self, **defaults):
+    def __init__(self, **defaults) -> None:
         self.defaults = defaults
 
-    def __call__(self, func):
+    def __call__(self, func: Callable) -> Callable:
         sig = signature(func)
         sig_new = self.modify_signature(sig)
 
@@ -37,7 +44,7 @@ class set_defaults:
 
         return wrapper
 
-    def modify_signature(self, sig):
+    def modify_signature(self, sig: Signature) -> Signature:
         params = []
 
         for param in sig.parameters.values():
@@ -59,25 +66,24 @@ class set_defaults:
         return sig.replace(parameters=params)
 
 
-# utility functions
-def save_dataset(dataset, path):
+def save_dataset(dataset: xr.Dataset, path: PathLike) -> None:
     """Save dataset to a netCDF."""
     dataset.to_netcdf(path)
 
 
-def load_dataset(path):
+def load_dataset(path: PathLike) -> xr.Dataset:
     """Load a netCDF to a dataset."""
     return xr.open_dataset(path)
 
 
-def random_hex(length=8):
+def random_hex(length: int = 8) -> str:
     """Random hexadecimal string of given length."""
     return f"{getrandbits(length*4):x}"
 
 
-def runner(n_procs=None):
+def runner(n_procs: Optional[int] = None) -> ProcessPoolExecutor:
     """Multiprocessing task runnner."""
     if n_procs is None:
         n_procs = cpu_count() - 1
 
-    return futures.ProcessPoolExecutor(n_procs)
+    return ProcessPoolExecutor(n_procs)
